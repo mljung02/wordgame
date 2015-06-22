14621cb986b1b071f45298aae09aa1f991171551
Mattias Ljung <mljung02@gmail.com>
2015-06-22 13:02:11 -0600

    working wordmatch, basic score

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,11 +3,11 @@ var wordbox = document.getElementById('wordbox');
 var word = document.getElementById('word');
 var spin = document.getElementById('spin');
 var enter = document.getElementById('enter');
-// var dictionaryArray = dictionary.split("\n");
+var score = document.getElementById('score');
 
 spin.addEventListener("click", function(){
+  wordbox.innerHTML = "";
   for (var i = 0; i < letterGen; i++) {
-    
     var letter = document.createElement('div');
     letter.className = "letter"
     letter.innerHTML = alph[Math.floor(Math.random() * alph.length)]
@@ -26,13 +26,13 @@ wordbox.addEventListener('click', select)
 
 console.log(dictionary[15000])
 
-// enter.addEventListener('click', function(){
-//   for (var i = 0; i < dictionaryArray.length; i++) {
-//     if (dictionaryArray[i] === word.innerHTML) {
-//       console.log('real word!')
-//     }
-//     else {
-//       console.log('word not found!')
-//     }
-//   }
-// })
+enter.addEventListener('click', function(){
+  for (var i = 0; i < dictionary.length; i++) {
+    if (dictionary[i] === word.innerHTML.toUpperCase()) {
+      console.log('real word!');
+      word.innerHTML = "";
+      score.innerHTML = parseInt(score.innerHTML) + 1
+      continue;
+    }
+  }
+})
