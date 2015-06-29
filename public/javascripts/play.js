ef2faf0ad233ed391bd98666affc4898c7c5f1e2
Mattias Ljung <mljung02@gmail.com>
2015-06-29 11:20:29 -0600

    moving level one boss

--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -2,117 +2,122 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-context.fillRect(700, 0, 100, 300)
-
 canvas.width = 875;
 canvas.height = 300;
 
-var coinImage = new Image();
-coinImage.src = "/images/coin-sprite-animation.png";
-var batImage = new Image();
-batImage.src = '/images/vampires.png'
+// var batImage = new Image();
+// batImage.src = '/images/vampires.png'
+var bossOne = new Image();
+bossOne.src = '/images/levelone.png'
 
-function sprite (options) {
+//Robot Constructor
+var Robot = function Robot(options) {
 	
-		var that = {},
-			frameIndex = 0,
-			tickCount = 0,
-			ticksPerFrame = options.ticksPerFrame || 0,
-			numberOfFrames = options.numberOfFrames || 1,
-      xpos = 0,
-      direction = 1;
-		
-    
-    that.sheetX = options.sheetX;
-    that.sheetY = options.sheetY;
-		that.context = options.context;
-		that.width = options.width;
-		that.height = options.height;
-		that.image = options.image;
-		
-		that.update = function () {
+	var frameIndex;
+	
+	//inherent properties
+	this.frameIndex = 0,
+	this.tickCount = 0,
+	this.ticksPerFrame = options.ticksPerFrame || 0,
+	this.numberOfFrames = options.numberOfFrames || 1,
+	this.xpos = options.xpos || 25,
+	this.ypos = options.ypos || 200,
+	this.direction = 1;
+	this.dirIndex = 0;
+	
+	//Passed attributes
+	this.health = options.health;
+	this.fireRate = options.fireRate;
+	this.sheetX = options.sheetX;
+	this.sheetY = options.sheetY;
+	this.context = options.context;
+	this.width = options.width;
+	this.height = options.height;
+	this.image = options.image;
 
-            tickCount += 1;
-
-            if (tickCount > ticksPerFrame) {
-
-				tickCount = 0;
-				
-                // If the current frame index is in range
-                if (frameIndex < numberOfFrames - 1) {	
-                    // Go to the next frame
-                    frameIndex += 1;
-                } else {
-                    frameIndex = 0;
-                }
-            }
-            if (xpos > 700 || xpos < 0) {
-              direction *= -1;
-            }
-          xpos += 1 * direction;
-        };
-		
-		that.render = function () {
-		
-		  // Clear the canvas
-		  that.context.clearRect(-5 + xpos, 200, that.width[frameIndex] + 40, that.height[frameIndex] +40);
-		  
-		  // Draw the animation
-		  that.context.drawImage(
-		    that.image,
-		    // frameIndex * that.width / numberOfFrames,
-        that.sheetX[frameIndex],
-        // 0
-		    that.sheetY[frameIndex],
-		    that.width[frameIndex],
-		    that.height[frameIndex],
-		    xpos,
-		    200,
-        that.width[frameIndex],
-		    that.height[frameIndex]);
-		};
-		
-		return that;
-	}
-
-// var coin = sprite({
-//     context: canvas.getContext("2d"),
-//     width: 1000,
-//     height: 100,
-//     image: coinImage,
-//     numberOfFrames: 10,
-// 		ticksPerFrame: 10,
-// });
-
-var batObj = {
-  context: canvas.getContext("2d"),
-  width: [62, 75, 81, 74, 69, 52, 50, 51, 49, 52, 74],
-  height: [91, 91, 100, 91, 78, 68, 74, 69, 76, 78, 92],
-  image: batImage,
-  numberOfFrames: 11,
-  ticksPerFrame: 10,
-  sheetX: [17, 109, 196, 310, 414, 515, 596, 669, 744, 811, 881],
-  sheetY: [154, 153, 143, 138, 148, 166, 164, 157, 149, 148, 136]
 }
 
-var bat = sprite(batObj)
+Robot.prototype.render = function render(){
 
+	//img,sx,sy,sw,sh,dx,dy,dw,dh
+	this.context.drawImage(
+		this.image,
+		this.sheetX[this.dirIndex][this.frameIndex],
+		this.sheetY[this.dirIndex][this.frameIndex],
+		this.width[this.dirIndex][this.frameIndex],
+		this.height[this.dirIndex][this.frameIndex],
+		this.xpos,
+		this.ypos,
+		this.width[this.dirIndex][this.frameIndex] *.5,
+		this.height[this.dirIndex][this.frameIndex] *.5
+		);
+		
+}
+
+//Update frames
+Robot.prototype.update = function update(){
+	
+	//erase previous
+	this.context.clearRect(	this.xpos,
+													this.ypos,
+													this.width[this.dirIndex][this.frameIndex],
+													this.height[this.dirIndex][this.frameIndex])
+	
+	//updates pertinent positions
+	this.tickCount += 1;
+
+	if (this.tickCount > this.ticksPerFrame) {
+
+		this.tickCount = 0;
+
+			// If the current frame index is in range
+			if (this.frameIndex < this.numberOfFrames - 1) {	
+					// Go to the next frame
+					this.frameIndex += 1;
+			} else {
+					this.frameIndex = 0;
+			}
+	}
+	if (this.xpos > 800 || this.xpos < 25) {
+		this.dirIndex += this.direction;
+		this.direction *= -1;
+	}
+	this.xpos += 1 * this.direction;		
+}
+
+
+//TEST BATOBJ
+// var batObj = {
+//   context: canvas.getContext("2d"),
+//   width: [62, 75, 81, 74, 69, 52, 50, 51, 49, 52, 74],
+//   height: [91, 91, 100, 91, 78, 68, 74, 69, 76, 78, 92],
+//   image: batImage,
+//   numberOfFrames: 11,
+//   ticksPerFrame: 10,
+//   sheetX: [17, 109, 196, 310, 414, 515, 596, 669, 744, 811, 881],
+//   sheetY: [154, 153, 143, 138, 148, 166, 164, 157, 149, 148, 136]
+// }
+
+var bossOneAlObj = {
+	context: canvas.getContext("2d"),
+	width: [[109,59,80],[104,59,80]],
+	height: [[128,128,128],[127,127,127]],
+	sheetX: [[116,245,359],[119,248,362]],
+	sheetY: [[23,23,23],[173,173,173]],
+	image: bossOne,
+	numberOfFrames: 3,
+	ticksPerFrame: 8,
+}
+
+var bossOneAl =	new Robot(bossOneAlObj)
 
 function gameLoop () {
 
   window.requestAnimationFrame(gameLoop);
-  
-  bat.update();
-  bat.render();
+	// setTimeout(gameLoop, 100)
+	
+  bossOneAl.update();
+  bossOneAl.render();
 }
 
-function batRender () {
-  // context.drawImage(batImage, batObj.sheetX[0], batObj.sheetY[0], batObj.width[0], batObj.height[0], 0, 0, batObj.width[0], batObj.height[0]);
-  // context.drawImage(coinImage, 0, 0)
-  // context.drawImage(batImage, batObj.sheetX[0], batObj.sheetY[0], batObj.width, 68, 0, 0, 52, 68)
-}
-
-batImage.addEventListener("load", gameLoop);
-// window.addEventListener('load', function () {
-//   coin.render();
-// })
+bossOne.addEventListener("load", gameLoop);
